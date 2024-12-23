74896364826a9df53ec34ca0edd21f66c69cb507
KarolSU96 <karol96266@gmail.com>
2024-12-23 14:33:37 +0100

    Remove sec key. Add models

--- christmas_backend/settings.py
+++ christmas_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--eq103ie^cyc$gs95$_suz3kl&xd)%e8199nhxc=h!v@f4dbn9'
+SECRET_KEY = os.getenv('Django_SECRET_KEY')
+
+if not SECRET_KEY:
+    raise ValueError("No SECRET_KEY set for Django application.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'christmas_app',
 ]
 
 MIDDLEWARE = [
