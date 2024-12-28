2825c639bd82623deaf4ea3b716fa0714745b919
KarolSU96 <karol96266@gmail.com>
2024-12-28 09:51:59 +0100

    Add url and serializer to Profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Profile
+from .ProfileSerializer import ProfileSerializer
 
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 # Create your views here.
