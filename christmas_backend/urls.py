f1054d744c0c2cc157e8b01bcda33f6ab7513770
KarolSU96 <karol96266@gmail.com>
2024-12-30 12:06:21 +0100

    Add permissions to profiles

--- christmas_backend/urls.py
+++ christmas_backend/urls.py
@@ -23,5 +23,6 @@ router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api_auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
 ]
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,7 +4,11 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
 
+    def get_is_owner(self, obj):
+        return obj.owner == self.context['request'].user
+    
     class Meta:
         model = Profile
-        fields = ['id', 'owner', 'name', 'image', 'created_at', 'updated_at']
+        fields = ['id', 'owner', 'name', 'image', 'created_at', 'updated_at', 'is_owner']
