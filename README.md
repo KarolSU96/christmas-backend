d20ea6f125839b23937ec3e3938178b834813d71
KarolSU96 <karol96266@gmail.com>
2025-01-07 11:19:06 +0100

    Add default storage to to gifts

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # christmas-backend
 Backend for How to Christmas
+
+to do:
+- add generic views to existing views
+- add movies, activites models, views, serializers. Don't forget to register to admin.
--- gifts/models.py
+++ gifts/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
 
 class Gift(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(
-        upload_to='images/', default='../Gift-Placeholder_kivws5')
+        upload_to='images/',
+        storage=MediaCloudinaryStorage(),
+        default='../Gift-Placeholder_kivws5')
     price = models.DecimalField(max_digits=10, decimal_places=2,null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
