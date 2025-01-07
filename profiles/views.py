94c9c0de1d3c679f398932ff43ce7b7bbfbd6a4e
KarolSU96 <karol96266@gmail.com>
2025-01-07 11:30:27 +0100

    Add generics profile views

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import generics, status
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -6,35 +6,21 @@ from .models import Profile
 from .serializers import ProfileSerializer
 from christmas_backend.permissions import IsOwnerOrReadOnly
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
-        return Response(serializer.data)
+class ProfileList(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    
 # Create your views here.
 
 
-class ProfileDetail(APIView):
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
 
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile,context={'request': request})
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get_serializer_context(self):
+        """
+        Pass additional context to the serializer for use in validation or representation.
+        """
+        return {'request': self.request}
